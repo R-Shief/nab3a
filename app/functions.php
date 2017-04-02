cff1ac986a1e4fb27002c6eeea99f5314bdcb564
Benjamin Doherty <bjd@bangpound.org>
2017-04-02 04:54:48 -0500

    add app\debug function.

--- app/functions.php
+++ app/functions.php
@@ -2,8 +2,17 @@
 
 namespace app {
 
+    use Symfony\Component\Debug\Debug;
     use Symfony\Component\Dotenv\Dotenv;
     use Symfony\Component\Dotenv\Exception\PathException;
+
+	/**
+	 * Enables debugging during bootstrap.
+	 */
+    function debug()
+    {
+        Debug::enable();
+    }
 
     function dotenv_load()
     {
