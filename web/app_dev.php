7a0762cc591ad835f4d4c090e26df666170a8f46
Benjamin Doherty <bjd@bangpound.org>
2017-05-01 18:22:52 -0500

    symfony 3.3

--- web/app_dev.php
+++ web/app_dev.php
@@ -19,13 +19,15 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 }
 
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
+$loader = require __DIR__.'/../vendor/autoload.php';
 app\debug();
 app\dotenv_load();
 app\consul_load(include __DIR__.'/../app/config/consul_mapping.php');
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
+if (PHP_VERSION_ID < 70000) {
+    $kernel->loadClassCache();
+}
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
