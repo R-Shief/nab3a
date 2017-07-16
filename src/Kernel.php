c7953cb35d64bbd1fa97575dc96e7c0993b34afa
Benjamin Doherty <bjd@bangpound.org>
2017-07-16 13:53:42 -0500

    only check consul if not test.

--- src/Kernel.php
+++ src/Kernel.php
@@ -74,16 +74,18 @@ final class Kernel extends BaseKernel
         if (true === $this->booted) {
             return;
         }
-        $manager = (new ConsulEnvManagerBuilder())
-            ->withOverwriteEvenIfDefined(true)
-            ->build();
+        if ($this->environment !== 'test') {
+            $manager = (new ConsulEnvManagerBuilder())
+                ->withOverwriteEvenIfDefined(true)
+                ->build();
 
-        $manager->getEnvVarsFromConsul([
-            'TWITTER_CONSUMER_KEY' => 'twitter/consumer_key',
-            'TWITTER_CONSUMER_SECRET' => 'twitter/consumer_secret',
-            'TWITTER_ACCESS_TOKEN' => 'twitter/1/access_token',
-            'TWITTER_ACCESS_TOKEN_SECRET' => 'twitter/1/access_token_secret',
-        ]);
+            $manager->getEnvVarsFromConsul([
+                'TWITTER_CONSUMER_KEY' => 'twitter/consumer_key',
+                'TWITTER_CONSUMER_SECRET' => 'twitter/consumer_secret',
+                'TWITTER_ACCESS_TOKEN' => 'twitter/1/access_token',
+                'TWITTER_ACCESS_TOKEN_SECRET' => 'twitter/1/access_token_secret',
+            ]);
+        }
         parent::boot();
     }
 
